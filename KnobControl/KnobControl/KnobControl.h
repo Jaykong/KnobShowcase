c45360c5477da67ca428444f9470e8b6d668720f
Kong, Jay X. -ND <Jay.X.Kong.-ND@disney.com>
2018-10-17 17:43:42 +0800

    first

---
+++ KnobControl/KnobControl/KnobControl.h
@@ -0,0 +1,19 @@
+//
+//  KnobControl.h
+//  KnobControl
+//
+//  Created by Kong, Jay X. -ND on 2018/10/17.
+//  Copyright © 2018 Kong, Jay X. -ND. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for KnobControl.
+FOUNDATION_EXPORT double KnobControlVersionNumber;
+
+//! Project version string for KnobControl.
+FOUNDATION_EXPORT const unsigned char KnobControlVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <KnobControl/PublicHeader.h>
+
+
